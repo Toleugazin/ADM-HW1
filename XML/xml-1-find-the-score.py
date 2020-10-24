0800cfd2c5b0357162c7c3e2524b390101757e04
Toleugazin <68567693+Toleugazin@users.noreply.github.com>
2020-10-25 00:48:36 +0200

    Create xml-1-find-the-score.py

---
+++ XML/xml-1-find-the-score.py
@@ -0,0 +1,17 @@
+import xml.etree.ElementTree as etree
+
+def main():
+    line_num = int(input())
+    xml = ''
+    for i in range(line_num):
+        xml += input() + '\n'
+        
+    tree = etree.ElementTree(etree.fromstring(xml))
+    score = len(tree.getroot().attrib)
+    for element in tree.findall('.//*'):
+        score += len(element.attrib)
+    
+    print(score)
+    
+if __name__ == '__main__':
+    main()
